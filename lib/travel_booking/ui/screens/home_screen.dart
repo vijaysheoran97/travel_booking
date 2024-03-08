9d25e0823bfab554b7142ce66a83c1953f8b9452
Vijay Sheoran <vijaysirsi1997@gmail.com>
2024-03-08 15:38:12 +0530

    - minor changes

--- lib/travel_booking/ui/screens/home_screen.dart
+++ lib/travel_booking/ui/screens/home_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:travel_booking/travel_booking/ui/utils/app_color_constent.dart';
 
-
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
 
@@ -14,17 +13,29 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: Column(
-        children: [
+        children: [],
+      ),
+      appBar: AppBar(
+        backgroundColor: AppColorConstant.appBarColor,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.dark_mode_outlined,
+              color: AppColorConstant.appTextColor,
+            ),
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(
+              Icons.person_outline,
+              color: AppColorConstant.appTextColor,
+            ),
+          ),
         ],
       ),
-      appBar: AppBar(backgroundColor:AppColorConstant.appBarColor,actions: [
-        IconButton(onPressed: (){}, icon: const Icon(Icons.dark_mode_outlined,color: AppColorConstant.appTextColor,),),
-        IconButton(onPressed: (){}, icon: const Icon(Icons.person_outline,color: AppColorConstant.appTextColor,),),
-      ],),
       body: Column(
-        children: [
-
-        ],
+        children: [],
       ),
     );
   }
