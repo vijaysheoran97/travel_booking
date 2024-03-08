3c5010ad2920df945d48f59290ea8bbae6fe424a
Priyanka Sheoran <priynka.sheoran2004@gmail.com>
2024-03-08 14:44:09 +0530

    Implemented dark mode functionality in HomeScreen

--- lib/travel_booking/ui/screens/home_screen.dart
+++ lib/travel_booking/ui/screens/home_screen.dart
@@ -13,193 +13,203 @@ class _HomeScreenState extends State<HomeScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: AppColorConstant.appBarColor,
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.dark_mode_outlined,
-              color: AppColorConstant.appTextColor,
-            ),
-          ),
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.person_outline,
-              color: AppColorConstant.appTextColor,
-            ),
-          ),
-        ],
-      ),
-      drawer: Drawer(
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            topRight: Radius.circular(0),
-            bottomRight: Radius.circular(0),
-          ),
-        ),
-        child: ListView(
-          children: [
-            DrawerHeader(
-              decoration: const BoxDecoration(
-                color: AppColorConstant.appBarColor,
-              ),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  ClipOval(
-                    child: Image.asset(
-                      'assets/images/images.jpg',
-                      height: 50,
-                      width: 50,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                  const SizedBox(
-                    width: 12,
-                  ),
-                  const Text(
-                    'Angela Mayer\nVerified user.Membership',
-                    style: TextStyle(
-                      color: AppColorConstant.appTextColor,
-                      fontSize: 16,
-                    ),
-                  ),
-                ],
+    return MaterialApp(
+      theme: isDarkMode ? ThemeData.dark() : ThemeData.light(),
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: AppColorConstant.appBarColor,
+          actions: [
+            IconButton(
+              onPressed: () {
+                setState(() {
+                  isDarkMode = !isDarkMode;
+                });
+              },
+              icon: Icon(
+                isDarkMode
+                    ? Icons.light_mode_outlined
+                    : Icons.dark_mode_outlined,
+                color: AppColorConstant.appTextColor,
               ),
             ),
-            ListTile(
-              title: const Text(
-                'Menu',
-                style: TextStyle(
-                    color: AppColorConstant.appTextColorblack,
-                    fontWeight: FontWeight.w600),
-              ),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: AppColorConstant.appBarColor,
-                ),
-                height: 35,
-                width: 35,
-                child: const Icon(
-                  Icons.watch_later_outlined,
-                  color: AppColorConstant.appTextColor,
-                ),
-              ),
-              title: const Text('Overview'),
-              trailing: const Icon(Icons.arrow_forward_ios_outlined),
-              onTap: () {},
-            ),
-            const Divider(),
-            ListTile(
-              leading: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: AppColorConstant.appBarColor,
-                ),
-                height: 35,
-                width: 35,
-                child: const Icon(
-                  Icons.contact_page_outlined,
-                  color: AppColorConstant.appTextColor,
-                ),
-              ),
-              title: const Text('Pages'),
-              trailing: const Icon(Icons.arrow_forward_ios_outlined),
-              onTap: () {},
-            ),
-            const Divider(),
-            ListTile(
-              title: const Text(
-                'Others',
-                style: TextStyle(
-                    color: AppColorConstant.appTextColorblack,
-                    fontWeight: FontWeight.w600),
-              ),
-              onTap: () {},
-            ),
-            ListTile(
-              leading: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: AppColorConstant.appBarColor,
-                ),
-                height: 35,
-                width: 35,
-                child: const Icon(
-                  Icons.settings_input_component,
-                  color: AppColorConstant.appTextColor,
-                ),
-              ),
-              title: const Text('Components'),
-              trailing: const Icon(Icons.arrow_forward_ios_outlined),
-              onTap: () {},
-            ),
-            const Divider(),
-            ListTile(
-              leading: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: AppColorConstant.appBarColor,
-                ),
-                height: 35,
-                width: 35,
-                child: const Icon(
-                  Icons.settings,
-                  color: AppColorConstant.appTextColor,
-                ),
-              ),
-              title: const Text('Setting'),
-              trailing: const Icon(Icons.arrow_forward_ios_outlined),
-              onTap: () {},
-            ),
-            const Divider(),
-            ListTile(
-              leading: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  color: AppColorConstant.appBarColor,
-                ),
-                height: 35,
-                width: 35,
-                child: const Icon(
-                  Icons.dark_mode_outlined,
-                  color: AppColorConstant.appTextColor,
-                ),
-              ),
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text('Switch To Dark Mode'),
-                  Container(
-                    height: 20,
-                    width: 32,
-                    child: Switch(
-                      value: isDarkMode,
-                      onChanged: (value) {
-                        setState(() {
-                          isDarkMode = value;
-                        });
-                      },
-                    ),
-                  ),
-                ],
+
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.person_outline,
+                color: AppColorConstant.appTextColor,
               ),
             ),
-            const Divider(),
           ],
         ),
-      ),
-      body: const Column(
-        children: [
-          // Your body content here
-        ],
+        drawer: Drawer(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topRight: Radius.circular(0),
+              bottomRight: Radius.circular(0),
+            ),
+          ),
+          child: ListView(
+            children: [
+              DrawerHeader(
+                decoration: const BoxDecoration(
+                  color: AppColorConstant.appBarColor,
+                ),
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ClipOval(
+                      child: Image.asset(
+                        'assets/images/images.jpg',
+                        height: 50,
+                        width: 50,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    const SizedBox(
+                      width: 12,
+                    ),
+                    const Text(
+                      'Angela Mayer\nVerified user.Membership',
+                      style: TextStyle(
+                        color: AppColorConstant.appTextColor,
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              ListTile(
+                title: const Text(
+                  'Menu',
+                  style: TextStyle(
+                      color: AppColorConstant.appTextColorblack,
+                      fontWeight: FontWeight.w600),
+                ),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: AppColorConstant.appBarColor,
+                  ),
+                  height: 35,
+                  width: 35,
+                  child: const Icon(
+                    Icons.watch_later_outlined,
+                    color: AppColorConstant.appTextColor,
+                  ),
+                ),
+                title: const Text('Overview'),
+                trailing: const Icon(Icons.arrow_forward_ios_outlined),
+                onTap: () {},
+              ),
+              const Divider(),
+              ListTile(
+                leading: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: AppColorConstant.appBarColor,
+                  ),
+                  height: 35,
+                  width: 35,
+                  child: const Icon(
+                    Icons.contact_page_outlined,
+                    color: AppColorConstant.appTextColor,
+                  ),
+                ),
+                title: const Text('Pages'),
+                trailing: const Icon(Icons.arrow_forward_ios_outlined),
+                onTap: () {},
+              ),
+              const Divider(),
+              ListTile(
+                title: const Text(
+                  'Others',
+                  style: TextStyle(
+                      color: AppColorConstant.appTextColorblack,
+                      fontWeight: FontWeight.w600),
+                ),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: AppColorConstant.appBarColor,
+                  ),
+                  height: 35,
+                  width: 35,
+                  child: const Icon(
+                    Icons.settings_input_component,
+                    color: AppColorConstant.appTextColor,
+                  ),
+                ),
+                title: const Text('Components'),
+                trailing: const Icon(Icons.arrow_forward_ios_outlined),
+                onTap: () {},
+              ),
+              const Divider(),
+              ListTile(
+                leading: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: AppColorConstant.appBarColor,
+                  ),
+                  height: 35,
+                  width: 35,
+                  child: const Icon(
+                    Icons.settings,
+                    color: AppColorConstant.appTextColor,
+                  ),
+                ),
+                title: const Text('Setting'),
+                trailing: const Icon(Icons.arrow_forward_ios_outlined),
+                onTap: () {},
+              ),
+              const Divider(),
+              ListTile(
+                leading: Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: AppColorConstant.appBarColor,
+                  ),
+                  height: 35,
+                  width: 35,
+                  child: const Icon(
+                    Icons.dark_mode_outlined,
+                    color: AppColorConstant.appTextColor,
+                  ),
+                ),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text('Switch To Dark Mode'),
+                    Container(
+                      height: 20,
+                      width: 32,
+                      child: Switch(
+                        value: isDarkMode,
+                        onChanged: (value) {
+                          setState(() {
+                            isDarkMode = value;
+                          });
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Divider(),
+            ],
+          ),
+        ),
+        body: const Column(
+          children: [
+            // Your body content here
+          ],
+        ),
       ),
     );
   }
